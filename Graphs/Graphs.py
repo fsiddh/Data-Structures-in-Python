88faca88b8cf90bdcd8af044fef8a8460d879904
fsiddh <jasonshaw546@gmail.com>
2020-08-28 20:38:21 +0530

    Adjacency Matrix

--- Graphs/Graphs.py
+++ Graphs/Graphs.py
@@ -3,7 +3,7 @@ import numpy as np
 class Graph:
 
     def __init__(self, vertices):
-        self._vertices = vertices
+        self._vertices = vertices # To initialize number of vertices in the graph
         self._adjMat = np.zeros((vertices, vertices))
 
     def insert_edge(self, u, v, w=1):
@@ -13,7 +13,7 @@ class Graph:
         self._adjMat[u][v] = 0
     
     def exist_edge(self, u, v):
-        return self._adjMat != 0
+        return self._adjMat[u][v] != 0
 
     def vertex_count(self):
         return self._vertices
@@ -28,20 +28,21 @@ class Graph:
         return count
 
     def allvertices(self):
-        for i in range(self._vertives):
+        for i in range(self._vertices):
             print(i, end=' ')
         print()
     
     def edges(self):
         for i in range(self._vertices):
             for j in range(self._vertices):
-                print(i, '---', j)
+                if self._adjMat[i][j] != 0:
+                    print(i, '---', j)
             print()
     
     def outdegree(self, u):
         count = 0
         for j in range(self._vertices):
-            if self._adjMat[v][j] != 0:
+            if self._adjMat[u][j] != 0:
                 count += 1
         
         return count
@@ -53,3 +54,41 @@ class Graph:
                 count += 1
         
         return count
+
+    def display_adjMat(self):
+        print(self._adjMat)
+
+if __name__ == "__main__":
+    G = Graph(4)
+
+    print('Vertices: ', G.vertex_count())
+    G.display_adjMat()
+    print('Edges: ', G.edge_count(), '\n')
+
+    G.insert_edge(0, 1)
+    G.insert_edge(1, 0)
+
+    G.insert_edge(0, 2)
+    G.insert_edge(2, 0)
+
+    G.insert_edge(1, 2)
+    G.insert_edge(2, 1)
+
+    G.insert_edge(2, 3)
+    G.insert_edge(3, 2)
+  
+
+    G.display_adjMat()
+    print('Edges count: ', G.edge_count(), '\n')
+
+    # Display Edges
+    print('Edges:')
+    G.edges()
+
+    #Check if an edge exists
+    print('\n Existance of edge between 1 --- 2:', G.exist_edge(1, 2))
+    print('\n Existance of edge between 1 --- 3:', G.exist_edge(1, 3))
+
+    #Degree
+    print('\n In-Degree of vertex 2: ', G.indegree(2))
+    print('\n Out-Degree of vertex 2:', G.outdegree(2))
